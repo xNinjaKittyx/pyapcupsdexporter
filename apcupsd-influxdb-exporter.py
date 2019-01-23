003f121305b3f41022b9b62017eba6633214cd9e
Allan Tribe <atribe@ebrlabs.com>
2019-01-22 17:20:44 -0700

    Updated python to have less redundancy

--- apcupsd-influxdb-exporter.py
+++ apcupsd-influxdb-exporter.py
@@ -5,10 +5,10 @@ import time
 from apcaccess import status as apc
 from influxdb import InfluxDBClient
 
-HOSTNAME = os.getenv('HOSTNAME', 'apcupsd-influxdb-exporter')
+hostname = os.getenv('HOSTNAME', 'apcupsd-influxdb-exporter')
 dbname = os.getenv('INFLUXDB_DATABASE', 'apcupsd')
-user = os.getenv('INFLUXDB_USER', '')
-password = os.getenv('INFLUXDB_PASSWORD', '')
+user = os.getenv('INFLUXDB_USER')
+password = os.getenv('INFLUXDB_PASSWORD')
 port = os.getenv('INFLUXDB_PORT', 8086)
 host = os.getenv('INFLUXDB_HOST', 'localhost')
 client = InfluxDBClient(host, port, user, password, dbname)
@@ -19,14 +19,14 @@ while True:
     ups = apc.parse(apc.get(host=os.getenv('APCUPSD_HOST', 'localhost')), strip_units=True)
 
     if os.environ['WATTS']:
-        ups['NOMPOWER'] = os.environ['WATTS']
+        ups['NOMPOWER'] = os.getenv('WATTS')
 
-    watts = float(float(ups['NOMPOWER']) * float(0.01 * float(ups['LOADPCT'])))
+    watts = float(ups['NOMPOWER']) * 0.01 * float(ups['LOADPCT'])
     json_body = [
         {
             'measurement': 'apcaccess_status',
             'fields': {
-                'WATTS': float(watts),
+                'WATTS': watts,
                 'LOADPCT': float(ups['LOADPCT']),
                 'BCHARGE': float(ups['BCHARGE']),
                 'TONBATT': float(ups['TONBATT']),
@@ -38,7 +38,7 @@ while True:
                 'ITEMP': float(ups['ITEMP'])
             },
             'tags': {
-                'host': HOSTNAME
+                'host': hostname
             }
         }
     ]
