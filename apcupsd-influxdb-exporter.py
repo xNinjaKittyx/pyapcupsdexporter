95f09354f6825813749c6bda612459bb12d50e25
Allan Tribe <atribe@ebrlabs.com>
2019-02-18 22:13:16 -0700

    Now handle all the values from apcuspd. wrapped loop in trycatch so it keeps going

--- apcupsd-influxdb-exporter.py
+++ apcupsd-influxdb-exporter.py
@@ -10,42 +10,57 @@ dbname = os.getenv('INFLUXDB_DATABASE', 'apcupsd')
 user = os.getenv('INFLUXDB_USER')
 password = os.getenv('INFLUXDB_PASSWORD')
 port = os.getenv('INFLUXDB_PORT', 8086)
-host = os.getenv('INFLUXDB_HOST', 'localhost')
-client = InfluxDBClient(host, port, user, password, dbname)
+host = os.getenv('INFLUXDB_HOST')
+apcupsdHost = os.getenv('APCUPSD_HOST', host)
 
+delay = os.getenv('DELAY', 10)
+verbose = os.getenv('VERBOSE', 'false').lower() is 'false'
+
+
+removeTheseKeys = ['DATE', 'STARTTIME', 'END APC']
+tagKeys = ['APC', 'HOSTNAME', 'UPSNAME', 'VERSION', 'CABLE',  'MODEL', 'UPSMODE', 'DRIVER', 'APCMODEL', 'END APC']
+
+wattsKey = 'WATTS'
+nominalPowerKey = 'NOMPOWER'
+
+client = InfluxDBClient(host, port, user, password, dbname)
 client.create_database(dbname)
 
 while True:
-    ups = apc.parse(apc.get(host=os.getenv('APCUPSD_HOST', 'localhost')), strip_units=True)
+    try:
+        ups = apc.parse(apc.get(host=apcupsdHost), strip_units=True)
+        for key in removeTheseKeys:
+            del ups[key]
 
-    if os.environ['WATTS']:
-        ups['NOMPOWER'] = os.getenv('WATTS')
+        tags = {'host': hostname}
 
-    watts = float(ups['NOMPOWER']) * 0.01 * float(ups['LOADPCT'])
-    json_body = [
-        {
-            'measurement': 'apcaccess_status',
-            'fields': {
-                'WATTS': watts,
-                'LOADPCT': float(ups['LOADPCT']),
-                'BCHARGE': float(ups['BCHARGE']),
-                'TONBATT': float(ups['TONBATT']),
-                'TIMELEFT': float(ups['TIMELEFT']),
-                'NOMPOWER': float(ups['NOMPOWER']),
-                'CUMONBATT': float(ups['CUMONBATT']),
-                'BATTV': float(ups['BATTV']),
-                'OUTPUTV': float(ups['OUTPUTV']),
-                'ITEMP': float(ups['ITEMP'])
-            },
-            'tags': {
-                'host': hostname
+        for key in tagKeys:
+            if key in ups:
+                tags[key] = ups[key]
+                del ups[key]
+
+        if wattsKey not in os.environ and nominalPowerKey not in ups:
+            raise ValueError("Your UPS does not specify NOMPOWER, you must specify the max watts your UPS can produce.")
+        else:
+            ups[nominalPowerKey] = os.getenv(wattsKey)
+
+        ups[wattsKey] = float(ups[nominalPowerKey]) * 0.01 * float(ups['LOADPCT'])
+
+        json_body = [
+            {
+                'measurement': 'apcaccess_status',
+                'fields': ups,
+                'tags': tags
             }
-        }
-    ]
+        ]
+        if verbose:
+            print(json_body)
+            print(client.write_points(json_body))
+        else:
+            client.write_points(json_body)
 
-    if os.getenv('VERBOSE', 'false').lower() == 'true':
-        print(json_body)
-        print(client.write_points(json_body))
-    else:
-        client.write_points(json_body)
-    time.sleep(5)
+        time.sleep(delay)
+    except ValueError as valueError:
+        raise valueError
+    except Exception as e:
+        print(e)
